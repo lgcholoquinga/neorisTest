560722ef4969b32ee4d3b5136446bea4d64d4ad9
lgcholoquinga <luisg.choloquinga@gamil.com>
2024-07-27 16:51:13 -0500

    shoe products by quamtity

--- src/app/products/components/table-foot/table-foot.component.ts
+++ src/app/products/components/table-foot/table-foot.component.ts
@@ -1,12 +1,23 @@
-import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-table-foot',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './table-foot.component.html',
   styleUrl: './table-foot.component.scss',
 })
 export class TableFootComponent {
-  totalProducts = input.required<number>();
+  public totalProducts = input.required<number>();
+  public onShowItems = output<number>();
+  public currentPage = 5;
+
+  /**
+   * Method that emit el new filter for show the items in the table.
+   */
+  onChangePagination() {
+    this.onShowItems.emit(this.currentPage);
+  }
 }
