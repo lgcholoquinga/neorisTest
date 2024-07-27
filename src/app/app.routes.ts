aaa8386c2db2e8b891dc385bb6b9565f9df907f1
lgcholoquinga <luisg.choloquinga@gamil.com>
2024-07-26 20:00:40 -0500

    Init development products

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('@products/products.routes').then((r) => r.PRODUCTS_ROUTES),
+  },
+  {
+    path: '404',
+    loadComponent: () => import('@shared/pages/not-found/not-found.component'),
+  },
+  {
+    path: '',
+    redirectTo: '/products',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: '/404',
+  },
+];
